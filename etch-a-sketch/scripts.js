b6566f77718d0eb4f9625c3f08acb17c3d37eba8
Amanda Pettenati <amanda@pette.net>
2021-03-10 16:15:07 -0500

    add documentation

--- etch-a-sketch/scripts.js
+++ etch-a-sketch/scripts.js
@@ -1,25 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
   const clearButton = document.querySelector('.clear')
   const sizeButton = document.querySelector('.size')
-
+  const slider = document.querySelector('.slider')
+  // set sizeButton text
   sizeButton.innerText = sizeButton.name
-
+  // create initial grid
   createGrid()
-
+  // add event listeners for buttons
   clearButton.addEventListener('click', clearScreen)
   sizeButton.addEventListener('click', changeSize)
-
-  const slider = document.querySelector('.slider')
+  // initialize slider position and color
   slider.value = 1
   slider.className = 'slider purple'
+  // add event listener for slider
   slider.addEventListener('click', draw)
 })
 
+// add event listener to draw
 window.addEventListener('mouseover', draw)
 
 function createGrid () {
   const sketchGrid = document.querySelector('.sketch-grid')
+  // remove existing grid elements
   sketchGrid.innerHTML = ''
+
+  // set sketch grid size variable based on size button name
   const size = document.querySelector('.size').name
   sketchGrid.className = `sketch-grid ${size}`
   const range = {
@@ -27,6 +32,9 @@ function createGrid () {
     medium: 30,
     large: 10
   }
+  document.documentElement.style.setProperty('--sketch-grid-item-size', range[size])
+
+  // create child div for each grid item based on grid size
   for (let i = 0; i < range[size] ** 2; i++) {
     const gridDiv = document.createElement('div')
     gridDiv.className = 'sketch-grid-item'
@@ -43,7 +51,9 @@ function draw (event) {
     4: 'green',
     5: 'blue'
   }
+  // set draw color based on slider value
   slider.className = `slider ${colors[slider.value]}`
+  // listen only for the sketch grid and draw if mouse is inside
   if (event.target.classList.contains('sketch-grid-item')) {
     event.target.className = `sketch-grid-item ${colors[slider.value]}`
   }
@@ -51,12 +61,14 @@ function draw (event) {
 
 function clearScreen () {
   const gridItems = document.querySelectorAll('.sketch-grid-item')
+  // reset class to remove background color classes
   gridItems.forEach((item) => {
     item.className = 'sketch-grid-item'
   })
 }
 
 function changeSize (event) {
+  // change button name and text on click
   const button = event.target
   if (button.name === 'small') {
     button.name = 'medium'
@@ -66,6 +78,7 @@ function changeSize (event) {
     button.name = 'small'
   }
   button.innerText = button.name
+  // clear screen and create grid based on new size
   clearScreen()
   createGrid()
 }
